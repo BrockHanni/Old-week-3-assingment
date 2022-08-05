35ae0be7af47b9bea546c019ecc6f7ef4ae314ba
BrockHanni <hannibr26@gmail.com>
2022-08-04 20:48:38 -0700

    Finished

--- assets/script.js
+++ assets/script.js
@@ -16,37 +16,93 @@ var specialChar =  [ "@","%","+","/","'","!","#","$","^","?",":",",",")","(","}"
 function getPasswordOptions() {
   var length = parseInt(
     prompt("Choose your character count! (8-128)"),
-    10
+    
   );
-
   if (Number.isNaN(length)) {
     alert("Please provide a valid number");
-    return null;
+    return
   }
-
   if (length < 8) {
     alert("Length must be 8-128");
-    return null;
+    return
   }
-
   if (length > 128) {
     alert("Length must be 8-128");
-    return null;
+    return
+  }
+  var specialcharinclude = confirm("Click 'OK' to add special characters.");
+  var numbersCharinclude = confirm("Click 'OK' to add numbers.");
+  var lowlettersCharinclude = confirm("Click 'OK' to add lowercase letters." );
+  var uplettersCharinclude = confirm("Click 'OK' to add uppercase letters.");
+  if (
+    specialcharinclude === false &
+    numbersCharinclude === false &
+    lowlettersCharinclude === false &
+    uplettersCharinclude === false
+  ) {
+    alert("Select at least one character type");
+    return ;
+  }
+  var passwordOptions = {
+    length: length,
+    specialcharinclude: specialcharinclude,
+    numbersCharinclude: numbersCharinclude,
+    lowlettersCharinclude: lowlettersCharinclude,
+    uplettersCharinclude: uplettersCharinclude,
+  };
+  return passwordOptions;
+}
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+function generatePassword() {
+  var options = getPasswordOptions();
+  var result = [];
+  var possibleCharacters = [];
+  var guaranteedCharacters = [];
+
+  if (!options) return "";
+
+
+  if (options.specialcharinclude) {
+    possibleCharacters = possibleCharacters.concat(specialChar);
+    guaranteedCharacters.push(getRandom(specialChar));
+  }
+  if (options.numbersCharinclude) {
+    possibleCharacters = possibleCharacters.concat(numbersChar);
+    guaranteedCharacters.push(getRandom(numbersChar));
+  }
+  if (options.lowlettersCharinclude) {
+    possibleCharacters = possibleCharacters.concat(lowlettersChar);
+    guaranteedCharacters.push(getRandom(lowlettersChar));
+  }
+  if (options.uplettersCharinclude) {
+    possibleCharacters = possibleCharacters.concat(uplettersChar);
+    guaranteedCharacters.push(getRandom(uplettersChar));
   }
 
-  var specialcharinclude = confirm(
-    "Click 'OK' to add special characters."
-  );
+ for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
 
-  var numbersCharinclude = confirm(
-    "Click 'OK' to add numbers."
-  );
-
-  var lowlettersCharinclude = confirm(
-    "Click 'OK' to add lowercase letters."
-  );
-
-  var uplettersCharinclude = confirm(
-    "Click 'OK' to add uppercase letters."
-  );
+    result.push(possibleCharacter);
   }
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+  return result.join("");
+}
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
